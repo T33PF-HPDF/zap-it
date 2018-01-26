fa7e38f3fd35eb031eaf7d981c8d3e2c7e623bbc
Joel Kingsley <joelkingsley.r@gmail.com>
2018-01-26 16:07:29 +0530

    Added create-row-in-gs

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,13 +1,41 @@
 from src import app
 from flask import jsonify
+from json import dumps
+from requests import post
 
+globalCreateRowWebhook = 'https://hooks.zapier.com/hooks/catch/2836701/8fmhzu/'
+globalCreateSpreadsheetWebhook = 'https://hooks.zapier.com/hooks/catch/2836701/8raumf/'
+globalUpdateRowWebhook = 'https://hooks.zapier.com/hooks/catch/2836701/8v1s1s/'
+globalDeleteRowWebhook = 'https://hooks.zapier.com/hooks/catch/2836701/8v1n8t/'
+
+sampleCreateRowInGSRequest = 
 
 @app.route("/")
 def home():
-    return "Hasura Hello World"
+    return "ZapIt : Backend! - Hello Global User"
 
 # Uncomment to add a new URL at /new
 
 @app.route("/json")
 def json_message():
-    return jsonify(message="Hello World")
+    return jsonify(message="Hello Global User")
+
+@app.route("/create-row-in-gs",methods=['POST'])
+def create_row_in_gs():
+    t_id = request.form['id']
+    t_name = request.form['name']
+    created_on = request.form['created_on']
+    modified_on = request.form['modified_on']
+    desc = request.form['desc']
+
+    create_row_data = {'id': str(t_id),'name':str(t_name),'created-on':str(created_on),'modified-on':str(modified_on),'desc':str(desc)}
+
+    response = requests.post(
+        globalCreateRowWebhook, data=json.dumps(create_row_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    print(response)
+    
+    
+    
+    
