29153f34b6f2e12f5bd371394dfc35edd8e2eb60
Joel Kingsley <joelkingsley.r@gmail.com>
2018-01-27 19:29:11 +0530

    changes

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -9,28 +9,22 @@ globalCreateRowWebhook = 'https://hooks.zapier.com/hooks/catch/2836701/8fmhzu/'
 def home():
     return "ZapIt : Backend! - Hello Global User"
 
-# Uncomment to add a new URL at /new
-
-@app.route("/json")
-def json_message():
-    return jsonify(message="Hello Global User")
-
 @app.route('/create-row-in-gs', methods=['GET','POST'])
 def create_row_in_gs():
-    if request.method == 'GET':
-        return make_response('Failure')
-    if request.method == 'POST':
-        t_id = request.json['id']
-        t_name = request.json['name']
-        created_on = request.json['created_on']
-        modified_on = request.json['modified_on']
-        desc = request.json['desc']
-        
-        create_row_data = {'id': str(t_id),'name':str(t_name),'created-on':str(created_on),'modified-on':str(modified_on),'desc':str(desc)}
-        
-        response = requests.post(
-            globalCreateRowWebhook, json=json.dumps(create_row_data),
-            headers={'Content-Type': 'application/json'},allow_redirects=False
-        )
-        return response.content
+    return str(request.method+" "+request.json['t_name'])
+##    if request.method == 'GET':
+##        return make_response('Failure')
+##    if request.method == 'POST':
+##        t_id = request.json['id']
+##        t_name = request.json['name']
+##        created_on = request.json['created_on']
+##        modified_on = request.json['modified_on']
+##        desc = request.json['desc']
+##        
+##        create_row_data = {'id': str(t_id),'name':str(t_name),'created-on':str(created_on),'modified-on':str(modified_on),'desc':str(desc)}
+##        
+####        response = requests.post(
+####            globalCreateRowWebhook, json=json.dumps(create_row_data),
+####            headers={'Content-Type': 'application/json'})
+##        return response.content
     
