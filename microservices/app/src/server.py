0118ff191b0d80291341b8fa32b5cc6ab26324a4
Joel Kingsley <joelkingsley.r@gmail.com>
2018-01-27 23:31:01 +0530

    changes

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -11,19 +11,23 @@ class CreateRow(Resource):
         return {'Parameters':'id,name,created_on,modified_on,desc in JSON format','Desc':'Creates a row in a Google spreadsheet.'}
     def put(self):
         if request.method == "PUT":
-            if request.form.get('id',"None") != "None":
-                row = {
-                    'id': request.form['id']
-                    ,'name': request.form['name']
-                    ,'created_on': request.form['created_on']
-                    ,'modified_on': request.form['modified_on']
-                    ,'desc': request.form['desc']}
-                url = 'https://hooks.zapier.com/hooks/catch/2836701/8fmhzu/'
-                response = post(
-                    url=url, json=dumps(row))
-                return response.text
+            if request.form.get('data','None') != 'None':
+                return request.form.get('data')
             else:
-                return "Parameters not binded"
+                return "Not binded"
+##            if request.form.get('id',"None") != "None":
+##                row = {
+##                    'id': request.form['id']
+##                    ,'name': request.form['name']
+##                    ,'created_on': request.form['created_on']
+##                    ,'modified_on': request.form['modified_on']
+##                    ,'desc': request.form['desc']}
+##                url = 'https://hooks.zapier.com/hooks/catch/2836701/8fmhzu/'
+##                response = post(
+##                    url=url, json=dumps(row))
+##                return response.text
+##            else:
+##                return "Parameters not binded"
         else:
             return "Bad Request: Not a PUT request"
 
