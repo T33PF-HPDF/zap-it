ba6b8041d62d020adcef33eded576f51211462d0
Joel Kingsley <joelkingsley.r@gmail.com>
2018-01-27 22:34:31 +0530

    changes

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -10,16 +10,19 @@ class CreateRow(Resource):
     def get(self):
         return {'Parameters':'id,name,created_on,modified_on,desc in JSON format','Desc':'Creates a row in a Google spreadsheet.'}
     def put(self):
-##        row = {
-##            'id': request.form['id']
-##            ,'name': request.form['name']
-##            ,'created_on': request.form['created_on']
-##            ,'modified_on': request.form['modified_on']
-##            ,'desc': request.form['desc']}
-##        url = 'https://hooks.zapier.com/hooks/catch/2836701/8fmhzu/'
-##        response = post(
-##            url=url, json=dumps(row))
-        return request.form['desc']
+        if request.method == "PUT" and request.form.get('id',"None") != "None":
+            row = {
+                'id': request.form['id']
+                ,'name': request.form['name']
+                ,'created_on': request.form['created_on']
+                ,'modified_on': request.form['modified_on']
+                ,'desc': request.form['desc']}
+            url = 'https://hooks.zapier.com/hooks/catch/2836701/8fmhzu/'
+            response = post(
+                url=url, json=dumps(row))
+            return response.text
+        else:
+            return "Bad Request"
 
 api.add_resource(CreateRow, '/CreateRow')
     
