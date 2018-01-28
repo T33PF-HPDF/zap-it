489b64daa8057c64b17dda7a50db0a116641d184
Joel Kingsley <joelkingsley.r@gmail.com>
2018-01-28 18:21:27 +0530

    changes

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -15,7 +15,7 @@ class CreateRow(Resource):
 ##            parser.add_argument('id', type=str, help='Test string')
 ##            args = parser.parse_args()
 ##            return args
-            return request.text
+            return request.json()
             
             if request.form.get('id',"None") != "None":
                 row = {
