650f275556862df94b48e717af35846612c7a635
Joel Kingsley <joelkingsley.r@gmail.com>
2018-01-28 20:20:54 +0530

    changes

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -11,17 +11,17 @@ class CreateRow(Resource):
         return {'Parameters':'id,name,created_on,modified_on,desc all in a dictionary','Desc':'Creates a row in a Google spreadsheet.'}
     def put(self):
         if request.method == "PUT":
-            return request.get_json()
+            return request.data.get('id','None')
         
-            parser = reqparse.RequestParser()
-            parser.add_argument('id', type=str, help='ID')
-            parser.add_argument('name', type=str, help='Name')
-            parser.add_argument('created_on', type=str, help='Created on')
-            parser.add_argument('modified_on', type=str, help='Last Modified on')
-            parser.add_argument('desc', type=str, help='Description')
-            args = parser.parse_args()
-            
-            return args
+##            parser = reqparse.RequestParser()
+##            parser.add_argument('id', type=str, help='ID')
+##            parser.add_argument('name', type=str, help='Name')
+##            parser.add_argument('created_on', type=str, help='Created on')
+##            parser.add_argument('modified_on', type=str, help='Last Modified on')
+##            parser.add_argument('desc', type=str, help='Description')
+##            args = parser.parse_args()
+##            
+##            return args
             
             if request.form.get('id',"None") != "None":
                 row = {
