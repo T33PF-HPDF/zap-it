61e6041634460c521437252e23caa909bd9d8822
Joel Kingsley <joelkingsley.r@gmail.com>
2018-01-28 08:52:25 +0530

    changes

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -11,26 +11,22 @@ class CreateRow(Resource):
         return {'Parameters':'id,name,created_on,modified_on,desc in JSON format','Desc':'Creates a row in a Google spreadsheet.'}
     def put(self):
         if request.method == "PUT":
-            if request.values.get('data','None') != 'None':
-                return request.values.get('data')
+            parser = reqparse.RequestParser()
+            parser.add_argument('data', type=str, help='Test string')
+            args = parser.parse_args(strict=True)
+            if request.form.get('id',"None") != "None":
+                row = {
+                    'id': request.form['id']
+                    ,'name': request.form['name']
+                    ,'created_on': request.form['created_on']
+                    ,'modified_on': request.form['modified_on']
+                    ,'desc': request.form['desc']}
+                url = 'https://hooks.zapier.com/hooks/catch/2836701/8fmhzu/'
+                response = post(
+                    url=url, json=dumps(row))
+                return response.text
             else:
-                parser = reqparse.RequestParser()
-                parser.add_argument('data', type=str, help='Test string')
-                args = parser.parse_args(strict=True)
                 return args
-##            if request.form.get('id',"None") != "None":
-##                row = {
-##                    'id': request.form['id']
-##                    ,'name': request.form['name']
-##                    ,'created_on': request.form['created_on']
-##                    ,'modified_on': request.form['modified_on']
-##                    ,'desc': request.form['desc']}
-##                url = 'https://hooks.zapier.com/hooks/catch/2836701/8fmhzu/'
-##                response = post(
-##                    url=url, json=dumps(row))
-##                return response.text
-##            else:
-##                return "Parameters not binded"
         else:
             return "Bad Request: Not a PUT request"
 
